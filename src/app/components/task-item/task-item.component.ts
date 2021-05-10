5f9063838c6b0de44ad6e63ac1ff6799ba1a47a7
Zabi Azimy <zabi.azimy@gmail.com>
2021-05-10 22:38:44 +0530

    first

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { Task } from 'src/app/Tasks';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task) {
+    this.onToggleReminder.emit(task);
+  }
+
+
+
+
+}
